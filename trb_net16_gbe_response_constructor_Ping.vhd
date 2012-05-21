1dedb4f6060d47d61dfd566c739a90587e7acddd
grzegorzkorcyl <Grzegorz@.(none)>
2012-05-21 11:27:29 +0200

    update

--- trb_net16_gbe_response_constructor_Ping.vhd
+++ trb_net16_gbe_response_constructor_Ping.vhd
@@ -300,8 +300,6 @@ TC_SRC_MAC_OUT    <= g_MY_MAC;
 TC_SRC_IP_OUT     <= g_MY_IP;
 TC_SRC_UDP_OUT    <= x"0000";  -- not used
 TC_IP_PROTOCOL_OUT <= X"01"; -- ICMP
-TC_IP_SIZE_OUT    <= (others => '0');  -- doesn't matter
-TC_UDP_SIZE_OUT   <= (others => '0'); -- doesn't matter
 TC_FLAGS_OFFSET_OUT <= (others => '0');  -- doesn't matter
 
 ADDR_PROC : process(CLK)
