983f75da700e3084e4b2bc0030a1069b0d90f7b7
grzegorzkorcyl <Grzegorz@.(none)>
2012-06-22 18:36:34 +0200

    update

--- trb_net16_gbe_response_constructor_SCTRL.vhd
+++ trb_net16_gbe_response_constructor_SCTRL.vhd
@@ -176,13 +176,21 @@ begin
 	if rising_edge(CLK) then
 		GSC_INIT_DATA_OUT(7 downto 0)  <= rx_fifo_q(16 downto 9);
 		GSC_INIT_DATA_OUT(15 downto 8) <= rx_fifo_q(7 downto 0);	
-		GSC_INIT_DATAREADY_OUT  <= gsc_init_dataready;
+		
+		if (GSC_INIT_READ_IN = '1' and dissect_current_state = LOAD_TO_HUB) or (dissect_current_state = WAIT_FOR_HUB) then
+			gsc_init_dataready <= '1';
+		else
+			gsc_init_dataready <= '0';
+		end if;
+		
 	end if;
 end process INIT_DATA_OUT_PROC;
 
 GSC_INIT_PACKET_NUM_OUT <= packet_num;
-gsc_init_dataready <= '1' when (GSC_INIT_READ_IN = '1' and dissect_current_state = LOAD_TO_HUB) or
-								(dissect_current_state = WAIT_FOR_HUB) else '0';
+--gsc_init_dataready <= '1' when (GSC_INIT_READ_IN = '1' and dissect_current_state = LOAD_TO_HUB) or
+--								(dissect_current_state = WAIT_FOR_HUB) else '0';
+
+GSC_INIT_DATAREADY_OUT  <= gsc_init_dataready;
 
 transmit_fifo : fifo_65536x18x9
   PORT map(
