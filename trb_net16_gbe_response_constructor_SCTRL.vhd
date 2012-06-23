0ef2f1a4999710598f92d48947be9fccc6bdb208
grzegorzkorcyl <Grzegorz@.(none)>
2012-06-23 16:50:20 +0200

    update

--- trb_net16_gbe_response_constructor_SCTRL.vhd
+++ trb_net16_gbe_response_constructor_SCTRL.vhd
@@ -190,19 +190,33 @@ begin
 --		else
 --			gsc_init_dataready <= '0';
 --		end if;
---
+
 		GSC_INIT_DATAREADY_OUT  <= gsc_init_dataready;
 	end if;
 end process INIT_DATA_OUT_PROC;
 
 --GSC_INIT_DATA_OUT(7 downto 0)  <= rx_fifo_q(16 downto 9);
 --GSC_INIT_DATA_OUT(15 downto 8) <= rx_fifo_q(7 downto 0);	
-		
 
 GSC_INIT_PACKET_NUM_OUT <= packet_num;
 
 gsc_init_dataready <= '1' when (GSC_INIT_READ_IN = '1' and dissect_current_state = LOAD_TO_HUB) or
-								(dissect_current_state = WAIT_FOR_HUB and GSC_INIT_READ_IN = '0') else '0';
+							   (dissect_current_state = WAIT_FOR_HUB and GSC_INIT_READ_IN = '0') else '0';
+								
+PACKET_NUM_PROC : process(CLK)
+begin
+	if rising_edge(CLK) then
+		if (RESET = '1') or (dissect_current_state = IDLE) then
+			packet_num <= "100";
+--		elsif (GSC_INIT_READ_IN = '1' and gsc_init_dataready = '1' and packet_num = "100") then
+--			packet_num <= "000";
+		elsif (rx_fifo_rd = '1' and packet_num /= "100") then
+			packet_num <= packet_num + "1";
+		elsif (rx_fifo_rd = '1' and packet_num = "100") then
+			packet_num <= "000";
+		end if;
+	end if;
+end process PACKET_NUM_PROC;
 
 transmit_fifo : fifo_65536x18x9
   PORT map(
@@ -345,21 +359,6 @@ begin
 		end if;
 	end if;
 end process OFFSET_PROC;
-
-
-PACKET_NUM_PROC : process(CLK)
-begin
-	if rising_edge(CLK) then
-		if (RESET = '1') or (dissect_current_state = IDLE) then
-			packet_num <= "100";
-		elsif (GSC_INIT_READ_IN = '1' and gsc_init_dataready = '1' and packet_num = "100") then
-			packet_num <= "000";
-		elsif (rx_fifo_rd = '1' and packet_num /= "100") then
-			packet_num <= packet_num + "1";
-		end if;
-	end if;
-end process PACKET_NUM_PROC;
-
 
 DISSECT_MACHINE_PROC : process(CLK)
 begin
