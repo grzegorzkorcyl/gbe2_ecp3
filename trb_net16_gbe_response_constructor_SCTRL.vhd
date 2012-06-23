9e39f8315c0ca9ce2845e158bc5dbbb70e02e991
grzegorzkorcyl <Grzegorz@.(none)>
2012-06-23 16:07:14 +0200

    update

--- trb_net16_gbe_response_constructor_SCTRL.vhd
+++ trb_net16_gbe_response_constructor_SCTRL.vhd
@@ -169,12 +169,12 @@ rx_fifo_wr              <= '1' when PS_WR_EN_IN = '1' and PS_ACTIVATE_IN = '1' e
 --	end if;
 --end process RX_FIFO_RD_PROC;
 
-rx_fifo_rd <= '1' when (dissect_current_state = WAIT_FOR_HUB or dissect_current_state = LOAD_TO_HUB) else '0'; 
+--rx_fifo_rd <= '1' when (dissect_current_state = WAIT_FOR_HUB or dissect_current_state = LOAD_TO_HUB) and GSC_INIT_READ_IN = '1' else '0'; 
 
---rx_fifo_rd              <= '1' when (gsc_init_dataready = '1' and dissect_current_state = LOAD_TO_HUB) or 
---								(gsc_init_dataready = '1' and dissect_current_state = WAIT_FOR_HUB and GSC_INIT_READ_IN = '1') or
---								(dissect_current_state = READ_FRAME and PS_DATA_IN(8) = '1')
---								else '0';  -- preload first word
+rx_fifo_rd              <= '1' when (gsc_init_dataready = '1' and dissect_current_state = LOAD_TO_HUB) or 
+								(gsc_init_dataready = '1' and dissect_current_state = WAIT_FOR_HUB and GSC_INIT_READ_IN = '1') or
+								(dissect_current_state = READ_FRAME and PS_DATA_IN(8) = '1')
+								else '0';  -- preload first word
 
 INIT_DATA_OUT_PROC : process(CLK)
 begin
@@ -202,7 +202,7 @@ end process INIT_DATA_OUT_PROC;
 GSC_INIT_PACKET_NUM_OUT <= packet_num;
 
 gsc_init_dataready <= '1' when (GSC_INIT_READ_IN = '1' and dissect_current_state = LOAD_TO_HUB) or
-								(dissect_current_state = WAIT_FOR_HUB) else '0';
+								(dissect_current_state = WAIT_FOR_HUB and GSC_INIT_READ_IN = 0) else '0';
 
 transmit_fifo : fifo_65536x18x9
   PORT map(
