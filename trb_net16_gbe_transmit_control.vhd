0af01d119250c87191dd0032f06802c74073d49c
grzegorzkorcyl <grzegorz.korcy@gmail.com>
2012-11-15 19:21:03 +0100

    update

--- trb_net16_gbe_transmit_control.vhd
+++ trb_net16_gbe_transmit_control.vhd
@@ -156,14 +156,14 @@ begin
 	when IDLE =>
 		state <= x"1";
 		if (FC_READY_IN = '1') then
-			if (MC_TRANSMIT_DATA_IN = '1') then
-		  		tx_next_state <= TRANSMIT_DATA;
-		  	elsif (MC_TRANSMIT_CTRL_IN = '1') then
-		  		tx_next_state <= TRANSMIT_CTRL;
---			if (MC_TRANSMIT_CTRL_IN = '1') then
---		  		tx_next_state <= TRANSMIT_CTRL;
---			elsif (MC_TRANSMIT_DATA_IN = '1') then
+--			if (MC_TRANSMIT_DATA_IN = '1') then
 --		  		tx_next_state <= TRANSMIT_DATA;
+--		  	elsif (MC_TRANSMIT_CTRL_IN = '1') then
+--		  		tx_next_state <= TRANSMIT_CTRL;
+			if (MC_TRANSMIT_CTRL_IN = '1') then
+		  		tx_next_state <= TRANSMIT_CTRL;
+			elsif (MC_TRANSMIT_DATA_IN = '1') then
+		  		tx_next_state <= TRANSMIT_DATA;
 			else
 		  		tx_next_state <= IDLE;
 			end if;
