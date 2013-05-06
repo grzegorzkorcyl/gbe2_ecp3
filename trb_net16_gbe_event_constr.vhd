8bd23d8bd4f42371e447dc2011f66cde040f0aa6
grzegorzkorcyl <grzegorz.korcy@gmail.com>
2013-05-06 17:46:15 +0200

    update

--- trb_net16_gbe_event_constr.vhd
+++ trb_net16_gbe_event_constr.vhd
@@ -603,18 +603,13 @@ begin
 			termination(255 downto 8) <= termination(247 downto 0);
 			
 			case (load_current_state) is
-		
 				when PUT_Q_LEN => termination(7 downto 0) <= qsf_qq((header_ctr + 1) * 8 - 1  downto header_ctr * 8);
-				
 				when PUT_Q_DEC => termination(7 downto 0) <= PC_QUEUE_DEC_IN((header_ctr + 1) * 8 - 1  downto header_ctr * 8);
-				
 				when LOAD_SUB  => termination(7 downto 0) <= shf_qq;
-				
 				when LOAD_DATA => termination(7 downto 0) <= df_qq;
-				
-				when others    => TC_DATA_OUT <= (others => '0');
-		
+				when others    => termination(7 downto 0) <= (others => '0');
 			end case;
+			
 		else
 			termination <= termination;
 		end if;
