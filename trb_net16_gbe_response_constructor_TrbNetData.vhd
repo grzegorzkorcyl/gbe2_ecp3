168b873b253f64913e6ae871ceb67da5b5a1a340
grzegorzkorcyl <grzegorz.korcy@gmail.com>
2013-05-13 12:12:32 +0200

    update

--- trb_net16_gbe_response_constructor_TrbNetData.vhd
+++ trb_net16_gbe_response_constructor_TrbNetData.vhd
@@ -329,6 +329,7 @@ PS_BUSY_OUT <= '0' when dissect_current_state = IDLE else '1';
 PS_RESPONSE_READY_OUT <= '1' when (dissect_current_state = LOAD) or (dissect_current_state = WAIT_FOR_LOAD) else '0';
 
 TC_DATA_OUT           <= "0" & tc_data;
+TC_DATA_NOT_VALID_OUT <= tc_not_valid;
 TC_FRAME_SIZE_OUT 	  <= tc_ip_size + x"4";
 TC_IP_SIZE_OUT		  <= tc_ip_size;
 TC_UDP_SIZE_OUT		  <= tc_udp_size;
