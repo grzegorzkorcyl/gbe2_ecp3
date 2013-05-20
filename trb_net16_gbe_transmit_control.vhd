9a51780b43c6c4c585d4296740f7a6bf780540ff
grzegorzkorcyl <grzegorz.korcy@gmail.com>
2013-05-20 16:24:24 +0200

    update

--- trb_net16_gbe_transmit_control.vhd
+++ trb_net16_gbe_transmit_control.vhd
@@ -394,7 +394,7 @@ begin
 		--elsif (tx_current_state = CLEANUP and MC_FLAGS_OFFSET_IN(13) = '0') then
 --		elsif (tx_current_state = TRANSMIT_DATA and PC_EOD_IN = '1' and PC_FLAGS_OFFSET_IN(13) = '0') then
 --			sent_packets_ctr <= sent_packets_ctr + x"1";
-		elsif (tx_current_state = TRANSMIT_CTRL and ctrl_construct_current_state = CLOSE and MC_FLAGS_OFFSET_IN(13) = '0') then
+		elsif (tx_current_state = TRANSMIT_CTRL and ctrl_construct_current_state = CLOSE and MC_FLAGS_OFFSET_IN(13) = '0' and MC_DATA_NOT_VALID_IN = '0') then
 			sent_packets_ctr <= sent_packets_ctr + x"1";
 		end if;
 	end if;
