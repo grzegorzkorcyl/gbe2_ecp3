109527b8e983927ad3a1d7b56b069d916e7cc866
grzegorzkorcyl <grzegorz.korcy@gmail.com>
2013-06-11 12:45:16 +0200

    update

--- trb_net16_gbe_event_constr.vhd
+++ trb_net16_gbe_event_constr.vhd
@@ -746,7 +746,7 @@ begin
 		elsif (load_current_state = PRELOAD_Q) then
 			qsf_rd_en <= '1';
 		--elsif (load_current_state = PUT_Q_LEN and header_ctr = 2) then
-		elsif (load_current_state = PUT_Q_HEADERS and header_ctr > 0) then
+		elsif (TC_RD_EN_IN = '1' and load_current_state = PUT_Q_HEADERS and header_ctr > 0) then
 			qsf_rd_en <= '1';
 		else
 			qsf_rd_en <= '0';
