3d7cde0540e9cbcefac0986fcd21c71154d8adca
grzegorzkorcyl <grzegorz.korcy@gmail.com>
2013-06-13 11:24:38 +0200

    update

--- trb_net16_gbe_event_constr.vhd
+++ trb_net16_gbe_event_constr.vhd
@@ -450,7 +450,7 @@ begin
 			end if;
 			
 		when PRELOAD_Q =>
-			if (header_ctr = 8) then
+			if (header_ctr = 7) then
 				load_next_state <= WAIT_FOR_FC;
 			else
 				load_next_state <= PRELOAD_Q;
