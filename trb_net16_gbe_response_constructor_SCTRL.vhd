95500b7b1dba6f710fee5c20c7edb903a2fa541b
grzegorzkorcyl <grzegorz.korcy@gmail.com>
2013-07-03 23:31:08 +0200

    update

--- trb_net16_gbe_response_constructor_SCTRL.vhd
+++ trb_net16_gbe_response_constructor_SCTRL.vhd
@@ -210,7 +210,12 @@ begin
 		GSC_INIT_DATA_OUT(15 downto 8) <= rx_fifo_q(7 downto 0);
 		
 		GSC_INIT_PACKET_NUM_OUT <= packet_num;
-		GSC_INIT_DATAREADY_OUT  <= gsc_init_dataready;
+		
+		if (GSC_INIT_READ_IN = '1') then
+			GSC_INIT_DATAREADY_OUT <= '0';
+		else
+			GSC_INIT_DATAREADY_OUT <= gsc_init_dataready;
+		end if;
 	
 	end if;
 end process RX_FIFO_RD_SYNC;
