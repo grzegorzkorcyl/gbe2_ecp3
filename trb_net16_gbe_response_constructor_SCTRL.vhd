1167c3da101b086556c2413719c49a2a87dd15e7
grzegorzkorcyl <grzegorz.korcy@gmail.com>
2013-07-04 16:46:28 +0200

    update

--- trb_net16_gbe_response_constructor_SCTRL.vhd
+++ trb_net16_gbe_response_constructor_SCTRL.vhd
@@ -264,7 +264,7 @@ GSC_INIT_DATA_OUT(15 downto 8) <= rx_fifo_q(7 downto 0);
 -- TODO: change it to synchronous
 GSC_INIT_PACKET_NUM_OUT <= packet_num;
 GSC_INIT_DATAREADY_OUT <= gsc_init_dataready;
-gsc_init_dataready <= '1' when (GSC_INIT_READ_IN = '1' and dissect_current_state = LOAD_TO_HUB and rx_fifo_rd = '1') or
+gsc_init_dataready <= '1' when (GSC_INIT_READ_IN = '1' and dissect_current_state = LOAD_TO_HUB) or
 							   (dissect_current_state = WAIT_FOR_HUB) else '0';
 
 PACKET_NUM_PROC : process(CLK)
