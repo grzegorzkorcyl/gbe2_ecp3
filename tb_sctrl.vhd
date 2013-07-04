749697753428e7fb27e90a4fde48ef391061f003
grzegorzkorcyl <grzegorz.korcy@gmail.com>
2013-07-04 12:57:57 +0200

    update

--- tb_sctrl.vhd
+++ tb_sctrl.vhd
@@ -177,15 +177,15 @@ begin
 	wr_en <= '0';
 	activate <= '0';
 	
---	wait until rising_edge(dataready);
+	wait until rising_edge(dataready);
 --	wait until rising_edge(clk);
 --	read <= '1';
---	wait until rising_edge(clk);
---	wait until rising_edge(clk);
---	read <= '0';
---	wait until rising_edge(clk);
---	wait until rising_edge(clk);
---	read <= '1';
+	wait until rising_edge(clk);
+	wait until rising_edge(clk);
+	read <= '0';
+	wait until rising_edge(clk);
+	wait until rising_edge(clk);
+	read <= '1';
 	
 	wait;
 
