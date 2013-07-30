3749e6083786ac9fb0ffbcb3e84d569ca8f8b7e5
grzegorzkorcyl <grzegorz.korcy@gmail.com>
2013-07-30 15:01:38 +0200

    update

--- trb_net16_gbe_protocol_selector.vhd
+++ trb_net16_gbe_protocol_selector.vhd
@@ -244,10 +244,10 @@ port map (
 -- INTERFACE	
 	PS_DATA_IN		        => PS_DATA_IN,
 	PS_WR_EN_IN		        => PS_WR_EN_IN,
-	PS_ACTIVATE_IN		    => PS_PROTO_SELECT_IN(1),
-	PS_RESPONSE_READY_OUT	=> resp_ready(1),
-	PS_BUSY_OUT		        => busy(1),
-	PS_SELECTED_IN		    => selected(1),
+	PS_ACTIVATE_IN		    => PS_PROTO_SELECT_IN(0),
+	PS_RESPONSE_READY_OUT	=> resp_ready(0),
+	PS_BUSY_OUT		        => busy(0),
+	PS_SELECTED_IN		    => selected(0),
 	
 	PS_SRC_MAC_ADDRESS_IN	=> PS_SRC_MAC_ADDRESS_IN,
 	PS_DEST_MAC_ADDRESS_IN  => PS_DEST_MAC_ADDRESS_IN,
@@ -257,38 +257,38 @@ port map (
 	PS_DEST_UDP_PORT_IN	    => PS_DEST_UDP_PORT_IN,
 	 
 	TC_RD_EN_IN             => TC_RD_EN_IN,
-	TC_DATA_OUT		        => tc_data(2 * 9 - 1 downto 1 * 9),
-	TC_FRAME_SIZE_OUT	    => tc_size(2 * 16 - 1 downto 1 * 16),
-	TC_SIZE_LEFT_OUT        => tc_size_left(2 * 16 - 1 downto 1 * 16),
-	TC_FRAME_TYPE_OUT	    => tc_type(2 * 16 - 1 downto 1 * 16),
-	TC_IP_PROTOCOL_OUT	    => tc_ip_proto(2 * 8 - 1 downto 1 * 8),
-	TC_IDENT_OUT            => tc_ident(2 * 16 - 1 downto 1 * 16),
+	TC_DATA_OUT		        => tc_data(1 * 9 - 1 downto 0 * 9),
+	TC_FRAME_SIZE_OUT	    => tc_size(1 * 16 - 1 downto 0 * 16),
+	TC_SIZE_LEFT_OUT        => tc_size_left(1 * 16 - 1 downto 0 * 16),
+	TC_FRAME_TYPE_OUT	    => tc_type(1 * 16 - 1 downto 0 * 16),
+	TC_IP_PROTOCOL_OUT	    => tc_ip_proto(1 * 8 - 1 downto 0 * 8),
+	TC_IDENT_OUT            => tc_ident(1 * 16 - 1 downto 0 * 16),
 	 
-	TC_DEST_MAC_OUT		    => tc_mac(2 * 48 - 1 downto 1 * 48),
-	TC_DEST_IP_OUT		    => tc_ip(2 * 32 - 1 downto 1 * 32),
-	TC_DEST_UDP_OUT		    => tc_udp(2 * 16 - 1 downto 1 * 16),
-	TC_SRC_MAC_OUT		    => tc_src_mac(2 * 48 - 1 downto 1 * 48),
-	TC_SRC_IP_OUT		    => tc_src_ip(2 * 32 - 1 downto 1 * 32),
-	TC_SRC_UDP_OUT		    => tc_src_udp(2 * 16 - 1 downto 1 * 16),
+	TC_DEST_MAC_OUT		    => tc_mac(1 * 48 - 1 downto 0 * 48),
+	TC_DEST_IP_OUT		    => tc_ip(1 * 32 - 1 downto 0 * 32),
+	TC_DEST_UDP_OUT		    => tc_udp(1 * 16 - 1 downto 0 * 16),
+	TC_SRC_MAC_OUT		    => tc_src_mac(1 * 48 - 1 downto 0 * 48),
+	TC_SRC_IP_OUT		    => tc_src_ip(1 * 32 - 1 downto 0 * 32),
+	TC_SRC_UDP_OUT		    => tc_src_udp(1 * 16 - 1 downto 0 * 16),
 	
-	TC_IP_SIZE_OUT		    => tc_ip_size(2 * 16 - 1 downto 1 * 16),
-	TC_UDP_SIZE_OUT		    => tc_udp_size(2 * 16 - 1 downto 1 * 16),
-	TC_FLAGS_OFFSET_OUT	    => tc_flags_size(2 * 16 - 1 downto 1 * 16),
+	TC_IP_SIZE_OUT		    => tc_ip_size(1 * 16 - 1 downto 0 * 16),
+	TC_UDP_SIZE_OUT		    => tc_udp_size(1 * 16 - 1 downto 0 * 16),
+	TC_FLAGS_OFFSET_OUT	    => tc_flags_size(1 * 16 - 1 downto 0 * 16),
 	 
 	TC_BUSY_IN		        => TC_BUSY_IN,
 	
-	STAT_DATA_OUT           => stat_data(2 * 32 - 1 downto 1 * 32),
-	STAT_ADDR_OUT           => stat_addr(2 * 8 - 1 downto 1 * 8),
-	STAT_DATA_RDY_OUT       => stat_rdy(1),
-	STAT_DATA_ACK_IN        => stat_ack(1),
-	RECEIVED_FRAMES_OUT	    => RECEIVED_FRAMES_OUT(2 * 16 - 1 downto 1 * 16),
-	SENT_FRAMES_OUT		    => SENT_FRAMES_OUT(2 * 16 - 1 downto 1 * 16),
+	STAT_DATA_OUT           => stat_data(1 * 32 - 1 downto 0 * 32),
+	STAT_ADDR_OUT           => stat_addr(1 * 8 - 1 downto 0 * 8),
+	STAT_DATA_RDY_OUT       => stat_rdy(0),
+	STAT_DATA_ACK_IN        => stat_ack(0),
+	RECEIVED_FRAMES_OUT	    => RECEIVED_FRAMES_OUT(1 * 16 - 1 downto 0 * 16),
+	SENT_FRAMES_OUT		    => SENT_FRAMES_OUT(1 * 16 - 1 downto 0 * 16),
 -- END OF INTERFACE
 
 	DHCP_START_IN		    => DHCP_START_IN,
 	DHCP_DONE_OUT		    => DHCP_DONE_OUT,
 	 
-	DEBUG_OUT		        => PROTOS_DEBUG_OUT(2 * 32 - 1 downto 1 * 32)
+	DEBUG_OUT		        => PROTOS_DEBUG_OUT(1 * 32 - 1 downto 0 * 32)
  );
 
 -- protocol No. 3 Ping
