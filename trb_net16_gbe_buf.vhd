5f22b8ee5a6b9d1a6c59b7b2af65147138c7dd95
gk <grzegorz.korcyl@gmail.com>
2013-09-16 10:25:55 +0200

    update

--- trb_net16_gbe_buf.vhd
+++ trb_net16_gbe_buf.vhd
@@ -119,7 +119,7 @@ architecture trb_net16_gbe_buf of trb_net16_gbe_buf is
 --attribute HGROUP of trb_net16_gbe_buf : architecture is "GBE_BUF_group";
 
 
-component tsmac36 --tsmac35
+component tsmac35 --tsmac36 --tsmac35
 port(
 	--------------- clock and reset port declarations ------------------
 	hclk					: in	std_logic;
@@ -1342,7 +1342,7 @@ imp_gen: if (DO_SIMULATION = 0) generate
 	
 	
 	-- MAC part
-	MAC: tsmac36 --tsmac35
+	MAC: tsmac35 --tsmac36 --tsmac35
 	port map(
 	----------------- clock and reset port declarations ------------------
 		hclk				=> CLK,
