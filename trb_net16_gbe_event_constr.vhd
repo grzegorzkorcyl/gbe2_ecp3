6972561c02950157de203456ca6bfa6ca622aa25
gk <grzegorz.korcyl@gmail.com>
2013-09-19 17:17:28 +0200

    update

--- trb_net16_gbe_event_constr.vhd
+++ trb_net16_gbe_event_constr.vhd
@@ -574,8 +574,8 @@ end process TC_SOD_PROC;
 -- read from fifos
 
 df_rd_en <= '1' when (load_current_state = LOAD_DATA and TC_RD_EN_IN = '1') or 
-					(load_current_state = LOAD_SUB and header_ctr = 0 and TC_RD_EN_IN = '1') or 
-					(load_current_state = LOAD_SUB and header_ctr = 1 and TC_RD_EN_IN = '1')
+					(load_current_state = LOAD_SUB and header_ctr = 0 and TC_RD_EN_IN = '1') 
+					--(load_current_state = LOAD_SUB and header_ctr = 1 and TC_RD_EN_IN = '1')
 					else '0';
 
 shf_rd_en <= '1' when (load_current_state = LOAD_SUB and TC_RD_EN_IN = '1') or
