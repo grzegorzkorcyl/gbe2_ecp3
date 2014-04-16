63e7d5187dbdf9add343e09176dc3692d17ed9c3
gk <grzegorz.korcyl@gmail.com>
2014-04-16 11:48:44 +0200

    update

--- trb_net16_gbe_main_control.vhd
+++ trb_net16_gbe_main_control.vhd
@@ -590,7 +590,7 @@ begin
 			if (g_SIMULATE = 0) then
 				link_current_state <= INACTIVE;
 			else
-				link_current_state <= FINALIZE; --ACTIVE; --GET_ADDRESS; --ACTIVE;
+				link_current_state <= GET_ADDRESS; --FINALIZE; --ACTIVE; --GET_ADDRESS; --ACTIVE;
 			end if;
 		else
 			link_current_state <= link_next_state;
