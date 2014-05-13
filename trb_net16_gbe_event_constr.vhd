fefd6610eb8ee09b8307fa02405c844ca148c6fb
gk <grzegorz.korcyl@gmail.com>
2014-05-13 17:52:52 +0200

    update

--- trb_net16_gbe_event_constr.vhd
+++ trb_net16_gbe_event_constr.vhd
@@ -575,6 +575,8 @@ begin
 			end if;
 		elsif (load_current_state = GET_Q_SIZE) then
 			header_ctr <= header_ctr - 1;
+		elsif (load_current_state = LOAD_DATA and load_eod_q = '1' and end_queue_marker = '0') then
+			header_ctr <= 15;
 		else
 			header_ctr <= header_ctr;
 		end if;
