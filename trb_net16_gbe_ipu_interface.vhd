ace776e3610b23b36e31d8ba5da509362efe7a7b
gk <grzegorz.korcyl@gmail.com>
2014-05-18 00:40:27 +0200

    update

--- trb_net16_gbe_ipu_interface.vhd
+++ trb_net16_gbe_ipu_interface.vhd
@@ -517,7 +517,7 @@ begin
 		
 		when CLOSE_SUB =>
 			load_state <= x"9";
-			if (subevent_size > ("00" & x"7d00")) then
+			if (subevent_size > ("00" & x"5000")) then
 				load_next_state <= CLOSE_QUEUE; --WAIT_FOR_SUBS;
 			else
 				load_next_state <= WAIT_FOR_SUBS;
@@ -525,7 +525,7 @@ begin
 			
 		when CLOSE_QUEUE =>
 			load_state <= x"a";
-			if (subevent_size > ("00" & x"7d00")) then
+			if (subevent_size > ("00" & x"5000")) then
 				load_next_state <= WAIT_FOR_SUBS; --WAIT_FOR_SUBS;
 			else
 				load_next_state <= PREPARE_TO_LOAD_SUB;
@@ -558,7 +558,7 @@ begin
 	if rising_edge(CLK_GBE) then
 		if (load_current_state = IDLE) then
 			queue_size <= (others => '0');
-		elsif (load_current_state = CLOSE_QUEUE and subevent_size > ("00" & x"7d00")) then
+		elsif (load_current_state = CLOSE_QUEUE and subevent_size > ("00" & x"5000")) then
 			--queue_size <= subevent_size + x"10" + x"8" + x"4";
 			queue_size <= (others => '0');
 		elsif (load_current_state = WAIT_TWO) then
