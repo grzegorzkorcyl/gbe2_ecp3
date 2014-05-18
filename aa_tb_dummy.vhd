4a72d5c0952978d1b9c85681bbd0eb1f4d43330a
gk <grzegorz.korcyl@gmail.com>
2014-05-18 21:47:44 +0200

    update

--- aa_tb_dummy.vhd
+++ aa_tb_dummy.vhd
@@ -93,7 +93,7 @@ port(
 	BUS_ACK_OUT               : out std_logic;  -- gk 26.04.10
 	-- gk 23.04.10
 	LED_PACKET_SENT_OUT          : out std_logic;
-	LED_GBE_READY_OUT            : out std_logic;
+	LED_AN_DONE_N_OUT            : out std_logic;
 	-- CTS interface
 	CTS_NUMBER_IN				: in	std_logic_vector (15 downto 0);
 	CTS_CODE_IN					: in	std_logic_vector (7  downto 0);
@@ -268,7 +268,7 @@ port map(
 	BUS_ACK_OUT               => open,
 	-- gk 23.04.10
 	LED_PACKET_SENT_OUT         => open,
-	LED_GBE_READY_OUT           => gbe_ready,
+	LED_AN_DONE_N_OUT           => gbe_ready,
 	-- CTS interface
 	CTS_NUMBER_IN				=> cts_number_in,            
 	CTS_CODE_IN					=> cts_code_in,              
