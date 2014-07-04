b6b2505b99f72befc3b135801a69bc0a6941b759
gk <grzegorz.korcyl@gmail.com>
2014-07-04 11:38:25 +0200

    update

--- aa_tb_dummy.vhd
+++ aa_tb_dummy.vhd
@@ -227,7 +227,7 @@ generic map(
 	USE_125MHZ_EXTCLK       => 0,
 	
 		FIXED_SIZE_MODE => 1,
-		FIXED_SIZE => 350, --14000, --8832, --5000, --10000, --10,
+		FIXED_SIZE => 335, --14000, --8832, --5000, --10000, --10,
 		FIXED_DELAY_MODE => 1,
 		FIXED_DELAY => 100 --4096
 )
