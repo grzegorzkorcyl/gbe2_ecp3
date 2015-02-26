ee5a7e3c5bcbcf907d35384a4e1502d83a35bfd4
gk <grzegorz.korcyl@gmail.com>
2015-02-26 17:18:14 +0100

    update

--- gbe_wrapper.vhd
+++ gbe_wrapper.vhd
@@ -827,7 +827,7 @@ begin
 		generic map(
 			DO_SIMULATION          => DO_SIMULATION,
 			INCLUDE_DEBUG          => INCLUDE_DEBUG,
-			NUMBER_OF_OUTPUT_LINKS => 1
+			NUMBER_OF_OUTPUT_LINKS => 2
 		)
 		port map(
 			CLK_SYS_IN                  => CLK_SYS_IN,
