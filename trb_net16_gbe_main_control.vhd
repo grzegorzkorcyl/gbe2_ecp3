3dbd5d4ed0e158cdd69888f5e1e2c77c7c7dba44
gk <grzegorz.korcyl@gmail.com>
2015-03-17 15:38:35 +0100

    update

--- trb_net16_gbe_main_control.vhd
+++ trb_net16_gbe_main_control.vhd
@@ -650,7 +650,7 @@ lsm_sim_gen : if DO_SIMULATION = 1 generate
 	end process;
 end generate lsm_sim_gen;
 
-LINK_STATE_MACHINE : process(link_current_state, dhcp_done, wait_ctr, PCS_AN_COMPLETE_IN, 	MAC_READY_CONF_IN, tsm_ready, link_ok_timeout_ctr)
+LINK_STATE_MACHINE : process(link_current_state, dhcp_done, wait_ctr, PCS_AN_COMPLETE_IN, 	MAC_READY_CONF_IN, INCLUDE_DHCP, link_ok_timeout_ctr)
 begin
 	case link_current_state is
 		
@@ -699,7 +699,11 @@ begin
 				link_next_state <= INACTIVE;
 			else
 				if (wait_ctr = x"0000_1000") then
-					link_next_state <= GET_ADDRESS;
+					if (INCLUDE_DHCP = '1') then
+						link_next_state <= GET_ADDRESS;
+					else
+						link_next_state <= ACTIVE;
+					end if;
 				else
 					link_next_state <= WAIT_FOR_BOOT;
 				end if;
